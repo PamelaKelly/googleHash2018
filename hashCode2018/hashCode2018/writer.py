7935e7b6ea33d1bb63a5735f2a31d07308fd4fae
Cara Delorey <deloreyc@tcd.ie>
2018-03-01 19:40:38 +0000

    adding writer.py

--- hashCode2018/hashCode2018/writer.py
+++ hashCode2018/hashCode2018/writer.py
@@ -1,4 +1,13 @@
 class Writer:
-    
-    def __init__(self):
-        pass
+    #output = number of rides assigned, ride numbers
+    @staticmethod
+    def write_file(outputfile, carsList):
+        try:
+            f = open(outputfile, 'w')
+            #number of car objects in list
+            for car in range(0, len(carsList)):
+                #collating string of num rides + list of rides
+                output = str(car.returnNumRides()) + " " + str(car.returnAssignedRides())
+                print >> f, output
+        except IOError:
+            raise
