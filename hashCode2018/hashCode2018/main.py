05b5dc37cffaa029bf6090ce138c3a8aec4ac112
PamelaKelly <pamelakelly7@gmail.com>
2018-03-01 19:57:25 +0000

    updates

--- hashCode2018/hashCode2018/main.py
+++ hashCode2018/hashCode2018/main.py
@@ -3,7 +3,7 @@ from hashCode2018.car import Car
 from hashCode2018.ride import Ride
 def main():
     """ Top level function to run the program. """
-    
+
     #STEP 1: READ THE FILE
     data = Reader.read_file("#")
     program_details = data[0]
@@ -16,22 +16,22 @@ def main():
     bonus = program_details[4]
     steps = program_details[5]
     cars = []
-    
+
     # STEP 2: CREATE CARS
     for i in range(1, fleet_num + 1):
         car = Car(i)
         cars.append(car)
-        
+
     # STEP 3: CREATE RIDES & AVAILABLE RIDES
     for i in range(rides_num - 1):
         ride_details = Reader.parse_ride(rides[i])
         rides[i] = Ride(ride_details)
 
     #Car.set_available_rides(len(rides))
-    
+
     # STEP 3: MAIN FUNCTIONALITY
-    
-    
-    
-    
-    
+    for i in range(steps):
+        for car in cars:
+            if car.is_available:
+                car.findRide(rides)
+            car.move()
